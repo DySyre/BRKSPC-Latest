0f90e505c4206dca2228c5f57824efa15b5ee141
DySyre <xnamecf04@gmail.com>
2023-08-26 00:57:53 +0800

    Added notif bell script and MD5
    encryption and bell working now

--- dashboard.php
+++ dashboard.php
@@ -120,6 +120,71 @@ session_start();
     </header>
     <section class="home" style="background:#202834;">
 
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+
+    $(document).ready(function (){
+
+        $('.notification').load('Ajax/Notification.php');
+        $('.counter').text('0').hide();
+
+        var counter = 0;
+
+        $('#form-submit').on('submit', function(event){
+            event.preventDefault();
+            
+            var subject = $('#subject').val().trim();
+            var comment = $('#comment').val().trim();
+
+            $('#sub-error').text('');
+            $('#com-error').text('');
+
+            if(subject != '' && comment != ''){
+                
+                $.ajax({
+                    type: "POST",
+                    url: "Ajax/Ins_notification.php",
+                    data: { 'subject' : subject, 'comment' : comment },
+                    success: function (response) {
+                        var status = JSON.parse(response);
+                        if(status.status == 101){
+                            counter++;
+                            $('.counter').text(counter).show();
+                            $('.notification').load('Ajax/Notification.php');
+                            $("#form-submit").trigger("reset");
+                            console.log(status.msg);
+                        }
+                        else{
+                           console.log(status.msg);
+                        }
+                    }
+                });
+
+            }
+            else{
+            
+                if(subject == ''){
+                    $('#sub-error').text("Please Enter Subject");
+                }
+                if(comment == ''){
+                    $('#com-error').text("Please Enter Comment");
+                }
+            }
+
+        });
+
+        $('#noti_count').on('click',function (){
+            counter = 0;
+            $('.counter').text('0').hide();
+        });
+
+    });
+
+</script>
+
       <div class="swiper bg-slider">
         <div class="swiper-wrapper">
           <div class="swiper-slide dark-layer" >
