7057b5017b1224904f2d7e36283d90c9c07791e6
DySyre <xnamecf04@gmail.com>
2023-09-03 16:03:55 +0800

    last commit sa notification
    para umayos yung notif dropdown

--- Ajax/Notification.php
+++ Ajax/Notification.php
@@ -8,7 +8,7 @@ $select_status = $UDF_call->select_order_limit('notification', 'n_id', 10);
 ?>
 
     <?php if($select_status){ foreach($select_status as $se_noti){ ?>
-    <div class="dropdown" >
+    <div class="dropdown-item" >
         <h6><?php echo $se_noti['n_sub']; ?></h6>
         <span><?php echo $se_noti['n_msg']; ?></span>
         <hr class="mt-1 mb-1">
