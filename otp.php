60ce7117d91b9dfe921f203e4262a44aedca4bd2
Aizen <davidjonathantorres1@gmail.com>
2023-09-04 22:58:58 +0800

    Update Forgot pass
    Hindi pa totally okay yung forgot pass pero nakaka received na ng OTP

--- otp.php
+++ otp.php
@@ -1,5 +1,4 @@
-@@ -0,0 +1,39 @@
-<?php include_once ("controller.php"); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,26 +10,46 @@
 </head>
 <body>
     <div id="container">
-        <h2>Sign Up</h2>
+        <h2>Reset Password</h2>
         <p>It's quick and easy.</p>
         <div id="line"></div>
-        <form action="otp.php" method="POST" autocomplete="off">
+        <form action="#" method="POST" autocomplete="off">
             <?php
-            if(isset($_SESSION['message'])){
-                ?>
-                <div id="alert"><?php echo $_SESSION['message']; ?></div>
-                <?php
-            }
-            ?>
+            include("connect.php");
+            include("functions.php");
+            include("assets/libs/enc-dec/enc-dec.php");
+            use PHPMailer\PHPMailer\PHPMailer;
+            use PHPMailer\PHPMailer\SMTP;
+            use PHPMailer\PHPMailer\Exception;
+            require('assets/libs/email/PHPMailer/Exception.php');
+            require('assets/libs/email/PHPMailer/SMTP.php');
+            require('assets/libs/email/PHPMailer/PHPMailer.php');
+            // if Verify Button Clicked
+    if (isset($_POST['verify'])) {
+        $_SESSION['message'] = "";
+        $otp = mysqli_real_escape_string($con, $_POST['otp']);
+        $otp_query = "SELECT * FROM users_balagtas WHERE code = $otp";
+        $otp_result = mysqli_query($con, $otp_query);
 
-            <?php
-            if($errors > 0){
-                foreach($errors AS $displayErrors){
-                ?>
-                <div id="alert"><?php echo $displayErrors; ?></div>
-                <?php
-                }
+        if (mysqli_num_rows($otp_result) > 0) {
+            $fetch_data = mysqli_fetch_assoc($otp_result);
+            $fetch_code = $fetch_data['code'];
+
+            $update_status = "Active";
+            $update_code = 0;
+
+            $update_query = "UPDATE users_balagtas SET status = '$update_status' , code = $update_code WHERE code = $fetch_code";
+            $update_result = mysqli_query($con, $update_query);
+
+            if ($update_result) {
+                header('location: login.php');
+            } else {
+                $errors['db_error'] = "Failed To Insering Data In Database!";
             }
+        } else {
+            $errors['otp_error'] = "You enter invalid verification code!";
+        }
+    }
             ?>      
             <input type="number" name="otp" placeholder="Verification Code" required><br>
             <input type="submit" name="verify" value="Verify">
